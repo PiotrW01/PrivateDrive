70587ae96fa87297e41b7726515b9fd63d5e6676
PiotrW01 <piotr140401@gmail.com>
2025-05-29 21:30:25 +0200

    Add config loading

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,6 +1,30 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { ApplicationConfig} from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { AppConfigService } from './app/core/services/app-config.service';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+async function main() {
+  try {
+    const response = await fetch('/data/config.json');
+    if (!response.ok) throw new Error('Failed to load config.json');
+    const configData = await response.json();
+
+    const configService = new AppConfigService();
+    configService.load(configData);
+
+    const extendedAppConfig: ApplicationConfig = {
+      ...appConfig,
+      providers: [
+        { provide: AppConfigService, useValue: configService },
+        ...appConfig.providers,
+      ]
+    };
+
+    await bootstrapApplication(AppComponent, extendedAppConfig);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
